6415f887c7be16d45041ee8804a86b9add46d0f4
Ilya_Solov <7156643@gmail.com>
2024-02-26 00:15:44 +0300

    Swiper begin

---
+++ js/swiper.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        image: 'assets/picture.jpg',
+        title: 'Racing Drift 2',
+        desc: 'A 3D racing game, continuation of Racing Drift game',
+        tools: 'Unity, C#',
+    },
+    {
+        image: 'assets/picture.jpg',
+        title: 'Racing Drift 2',
+        desc: 'A 3D racing game, continuation of Racing Drift game',
+        tools: 'Unity, C#',
+    },
+    {
+        image: 'assets/picture.jpg',
+        title: 'Racing Drift 2',
+        desc: 'A 3D racing game, continuation of Racing Drift game',
+        tools: 'Unity, C#',
+    },
+    {
+        image: 'assets/picture.jpg',
+        title: 'Racing Drift 2',
+        desc: 'A 3D racing game, continuation of Racing Drift game',
+        tools: 'Unity, C#',
+    },
+    {
+        image: 'assets/picture.jpg',
+        title: 'Racing Drift 2',
+        desc: 'A 3D racing game, continuation of Racing Drift game',
+        tools: 'Unity, C#',
+    },
+];
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    freeMode: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
