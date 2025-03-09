16aafb2a7510258a3a7593414beac0b628addde8
Ilya_Solov <7156643@gmail.com>
2025-03-09 21:32:40 +0300

    Added images, fixes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,53 @@
 $(document).ready(function() {
     let dino = $('#dino');
-    let jumpHeight = 100;
-
-    let obstacleWidth = 50;
+    let jumpHeight = 130;
 
     let score = 0;
+    let lives = 3;
     let gameActive = false;
 
+    let obstacleWidth = 50;
+    let itemWidth = 30;
+    let obstacles = [];
+    let items = [];
+    let maxObstacles = 10;
     let obstacleInterval;
-    let obstacles = []; 
+    let itemInterval;
 
+    $('#score').hide();
+    $('#lives').hide();
     $('#startButton').click(function () {
         $('#gameStart').hide();
+        $('#score').show();
+        $('#lives').show();
 
         gameActive = true;
+        score = 0;
+        lives = 3;
+        obstacles = [];
+        items = [];
         obstacleInterval = setInterval(generateObstacle, 2000);
+        itemInterval = setInterval(generateItem, 3000);
+
+        $('.container').css('animation-play-state', 'running');
+        updateLives();
+        updateScore();
     });
 
     function updateScore() {
         $('#scoreValue').text(score);
+    }
+
+    function updateLives() {
+        $('#lives .life').removeClass('inactive');
+    
+        for (let i = 0; i < lives; i++) {
+            $('#life' + (i + 1)).addClass('active');
+        }
+    
+        for (let i = lives; i < 3; i++) {
+            $('#life' + (3 - i + 1)).addClass('inactive');
+        }
     }
 
     $(document).keydown(function(e) {
@@ -28,15 +57,24 @@ $(document).ready(function() {
         }
     });
 
+    function getRandomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
     function generateObstacle() {
+        if (score >= maxObstacles) {
+            gameOver(true);
+            return;
+        }
+
+        let multiplier = getRandomNumber(.65, 1);
         let obstacle = $('<div class="obstacle"></div>');
         obstacle.css({
             position: 'absolute',
             right: 0,
             bottom: 0,
-            width: obstacleWidth,
-            height: Math.random() * 50 + 30 + 'px',
-            backgroundColor: 'red'
+            width: 60 * multiplier,
+            height: 70 * multiplier
         });
         $('#obstacles').append(obstacle);
         obstacles.push(obstacle);
@@ -56,6 +94,33 @@ $(document).ready(function() {
         });
 
         checkCollision(obstacle);
+    }
+
+    function generateItem() {
+        if (lives >= 3) return;
+
+        let item = $('<div class="item"></div>');
+        item.css({
+            position: 'absolute',
+            right: 0,
+            bottom: Math.random() * 50 + 30 + 'px',
+            width: itemWidth,
+            height: itemWidth,
+            backgroundColor: 'gold'
+        });
+        $('#items').append(item);
+        items.push(item);
+
+        item.animate({ left: -itemWidth }, {
+            duration: 3000,
+            easing: 'linear',
+            complete: function() {
+                $(this).remove();
+                items = items.filter(i => i[0] !== this);
+            }
+        });
+
+        checkItemCollision(item);
     }
 
     function checkCollision(obstacle) {
@@ -80,23 +145,72 @@ $(document).ready(function() {
                 dinoPosition.top + dinoHeight > obstaclePosition.top
             ) {
                 clearInterval(obstacleInterval);
-                gameOver();
+                lives--;
+                obstacle.remove();
+                obstacle.stop();
+                updateLives();
+                if (lives <= 0) {
+                    gameOver(false);
+                }
             }
         }, 100);
     }
 
-    function gameOver() {
+    function checkItemCollision(item) {
+        let itemInterval = setInterval(function() {
+            if (!gameActive) {
+                clearInterval(itemInterval);
+                return;
+            }
+
+            let dinoPosition = dino.position();
+            let dinoWidth = dino.width();
+            let dinoHeight = dino.height();
+
+            let itemPosition = item.position();
+            let itemWidth = item.width();
+            let itemHeight = item.height();
+
+            if (
+                dinoPosition.left < itemPosition.left + itemWidth &&
+                dinoPosition.left + dinoWidth > itemPosition.left &&
+                dinoPosition.top < itemPosition.top + itemHeight &&
+                dinoPosition.top + dinoHeight > itemPosition.top
+            ) {
+                clearInterval(itemInterval);
+                if (lives < 3) {
+                    lives++;
+                    updateLives();
+                }
+                item.remove();
+            }
+        }, 100);
+    }
+
+    function gameOver(won) {
         gameActive = false;
         clearInterval(obstacleInterval);
-        
+        clearInterval(itemInterval);
+
         obstacles.forEach(obstacle => {
             obstacle.stop();
+        });
+
+        items.forEach(item => {
+            item.stop();
         });
 
         dino.stop();
         dino.css({ bottom: dino.position().bottom });
 
         $('#gameOver').show();
+        if (won) {
+            $('#gameOver h1').text("You won!");
+        } else {
+            $('#gameOver h1').text("Game Over");
+        }
+
+        $('.container').css('animation-play-state', 'paused');
     }
 
     $('#restartButton').click(function() {
